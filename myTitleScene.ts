701ace9929205d594546b4f142a21d6d5a9e8d75
David Brown <david@davidmarcbrown.com>
2021-04-25 18:44:37 -0400

    initial touch support

--- myTitleScene.ts
+++ myTitleScene.ts
@@ -28,6 +28,14 @@ export class MyTitleScene extends Phaser.Scene {
         this.add.text(128, 0, "LUDUM DARE 48").setOrigin(0.5, 0);
         this.add.text(128, 12, "DEEPER AND DEEPER").setOrigin(0.5, 0);
         this.add.text(128, 24, "@cheesepencil").setOrigin(0.5, 0);
+        this.input.on('pointerdown', () => {
+            console.log("pushed space...");
+            if (this.ready) {
+                console.log("pushed space AND ready...");
+                this.ready = false;
+                this.scene.start("MyGameScene", { level: 1 });
+            }
+        });
         this.input.keyboard.on("keydown-SPACE", () => {
             console.log("pushed space...");
             if (this.ready) {
